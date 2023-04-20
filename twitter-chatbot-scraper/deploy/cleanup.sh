fc1ac7e44b103a6c846ce637df640845264d7983
callbacked <117959902+callbacked@users.noreply.github.com>
2023-04-20 17:18:51 -0400

    this time

--- twitter-chatbot-scraper/deploy/cleanup.sh
+++ twitter-chatbot-scraper/deploy/cleanup.sh
@@ -9,30 +9,36 @@ ROLE_NAME="ElasticBeanstalkInstanceProfileRole"
 INSTANCE_PROFILE_NAME="ElasticBeanstalkInstanceProfile"
 S3_KEY="final.zip"
 
-# Terminate the Elastic Beanstalk environment
+echo -e "\033[33mInitiating deletion of Beanstalk environment\033[0m"
+
+
 aws elasticbeanstalk terminate-environment \
     --environment-name "$ENVIRONMENT_NAME" \
     --region "$REGION"
 
-# Wait for the environment to terminate
+echo -e "\033[33mWaiting for termination to complete, standby..\033[0m"
 aws elasticbeanstalk wait environment-terminated \
     --environment-name "$ENVIRONMENT_NAME" \
     --region "$REGION"
 
+echo -e "\033[32mBeanstalk environment deletion success [✓] \033[0m"
 
-# Delete the Elastic Beanstalk application version
+
+echo -e "\033[33mInitiating deletion of Beanstalk application\033[0m"
+
 aws elasticbeanstalk delete-application-version \
     --application-name "$APPLICATION_NAME" \
     --version-label "v1" \
     --delete-source-bundle \
     --region "$REGION"
 
-# Delete the Elastic Beanstalk application
+
 aws elasticbeanstalk delete-application \
     --application-name "$APPLICATION_NAME" \
     --region "$REGION"
+echo -e "\033[32mBeanstalk application deletion success [✓] \033[0m"
 
-# Delete the IAM role policies
+echo -e "\033[33mRemoving IAM role policies and instance profiles..\033[0m"
 aws iam detach-role-policy \
     --role-name "$ROLE_NAME" \
     --policy-arn arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier
@@ -45,6 +51,7 @@ aws iam detach-role-policy \
     --role-name "$ROLE_NAME" \
     --policy-arn arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier
 
+
 # Delete the IAM instance profile
 aws iam remove-role-from-instance-profile \
     --instance-profile-name "$INSTANCE_PROFILE_NAME" \
@@ -56,9 +63,13 @@ aws iam delete-instance-profile \
 # Delete the IAM role
 aws iam delete-role \
     --role-name "$ROLE_NAME"
+echo -e "\033[32mIAM roles and instance profiles successfully removed [✓] \033[0m"
 
-# Delete the S3 bucket objects and the bucket
+echo -e "\033[33mInitiating deletion of S3 Bucket and its contents\033[0m"
 aws s3 rm "s3://$S3_BUCKET_NAME/$S3_KEY"
 aws s3 rm "s3://$S3_BUCKET_NAME/bot-icon.png"
 aws s3 rb "s3://$S3_BUCKET_NAME" --force
+echo -e "\033[32mS3 Bucket successfully removed [✓] \033[0m"
 
+
+echo -e "\033[Deployment cleanup successful\033[0m"
--- twitter-chatbot-scraper/deploy/deploy.sh
+++ twitter-chatbot-scraper/deploy/deploy.sh
@@ -10,7 +10,7 @@ PLATFORM_VERSION="64bit Amazon Linux 2 v3.5.1 running Python 3.8"
 INSTANCE_PROFILE_NAME="ElasticBeanstalkInstanceProfile"
 ROLE_NAME="ElasticBeanstalkInstanceProfileRole"
 
-# Create S3 bucket
+echo -e "\033[33mCreating S3 Bucket and uploading contents\033[0m"
 if [ "$REGION" == "us-east-1" ]; then
   aws s3api create-bucket --bucket "$S3_BUCKET_NAME" --region "$REGION"
 else
@@ -21,27 +21,31 @@ fi
 aws s3 cp "./final.zip" "s3://$S3_BUCKET_NAME/$S3_KEY"
 aws s3 cp "./assets/bot-icon.png" "s3://twitter-chatbot-scraper-bucket"
 
-# Create Elastic Beanstalk application
-aws elasticbeanstalk create-application --application-name "$APPLICATION_NAME" --region "$REGION"
+echo -e "\033[32mS3 Bucket creation and content upload success [✓] \033[0m"
 
-# Create IAM role and instance profile
+echo -e "\033[33mCreating Beanstalk application\033[0m"
+aws elasticbeanstalk create-application --application-name "$APPLICATION_NAME" --region "$REGION"
+echo -e "\033[32mBeanstalk application creation success [✓] \033[0m"
+
+echo -e "\033[33mCreating IAM role and instance profile\033[0m"
 aws iam create-role --role-name "$ROLE_NAME" --assume-role-policy-document '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":{"Service":"ec2.amazonaws.com"},"Action":"sts:AssumeRole"}]}'
 aws iam create-instance-profile --instance-profile-name "$INSTANCE_PROFILE_NAME"
 aws iam add-role-to-instance-profile --role-name "$ROLE_NAME" --instance-profile-name "$INSTANCE_PROFILE_NAME"
+echo -e "\033[32mIAM role creation and instance profile success [✓] \033[0m"
 
-# Attach required policies to the IAM role
+echo -e "\033[33mAttaching policies\033[0m"
 aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier
 aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker
 aws iam attach-role-policy --role-name "$ROLE_NAME" --policy-arn arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier
+echo -e "\033[32mPolicies attached[✓] \033[0m"
 
-# Create Elastic Beanstalk application version
+echo -e "\033[33mFinalizing Beanstalk environment\033[0m"
 aws elasticbeanstalk create-application-version \
   --application-name "$APPLICATION_NAME" \
   --version-label "v1" \
   --source-bundle S3Bucket="$S3_BUCKET_NAME",S3Key="$S3_KEY" \
   --region "$REGION"
 
-# Create Elastic Beanstalk environment and deploy the application
 aws elasticbeanstalk create-environment \
   --application-name "$APPLICATION_NAME" \
   --environment-name "$ENVIRONMENT_NAME" \
@@ -50,3 +54,4 @@ aws elasticbeanstalk create-environment \
   --version-label "v1" \
   --option-settings Namespace=aws:elasticbeanstalk:application:environment,OptionName=PYTHON_ENV,Value=python3.8 \
                     Namespace=aws:autoscaling:launchconfiguration,OptionName=IamInstanceProfile,Value="$INSTANCE_PROFILE_NAME"
+echo -e "\033[32mDeployment success[✓] \033[0m"
